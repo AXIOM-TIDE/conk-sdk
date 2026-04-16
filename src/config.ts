6ede43a90c4c6c10dddebc086df6e08934b54d9e
Jason Jauert <jauert@axiomtide.com>
2026-04-16 19:36:12 +0000

    fix: export USDC_COIN_TYPE from config

--- src/config.ts
+++ src/config.ts
@@ -122,3 +122,5 @@ export const TREASURY_SHARE = 0.03   // 3% to protocol
  */
 export const SIREN_FLOOR_USDC  = 0.001
 export const SIREN_FLOOR_UNITS = 1_000
+
+export const USDC_COIN_TYPE = '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC'
